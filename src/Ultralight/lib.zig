c49385062a041f0ad86ba9ca02c0e8efe9c19cc4
Joel Gustafson <joelg@mit.edu>
2024-03-28 12:40:51 -0400

    add Bitmap, BitmapSurface, Rendertarget, and Surface exportts to Ultralight/lib.zig

--- src/Ultralight/lib.zig
+++ src/Ultralight/lib.zig
@@ -1,4 +1,8 @@
+pub const Bitmap = @import("Bitmap.zig");
+pub const BitmapSurface = @import("BitmapSurface.zig");
 pub const Config = @import("Config.zig");
 pub const Platform = @import("Platform.zig");
 pub const Renderer = @import("Renderer.zig");
+pub const RenderTarget = @import("RenderTarget.zig");
+pub const Surface = @import("Surface.zig");
 pub const View = @import("View.zig");
